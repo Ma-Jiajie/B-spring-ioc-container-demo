dd0b3742a78b13f9fae7d3ce3c5a15824805a3c9
Xiaofeng Wang <xfun68@gmail.com>
2020-05-21 09:56:53 +0800

    rename

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/DemoSpringIocContainerApplication.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/DemoSpringIocContainerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemospringioccontainerApplication {
+public class DemoSpringIocContainerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemospringioccontainerApplication.class, args);
+		SpringApplication.run(DemoSpringIocContainerApplication.class, args);
 	}
 
 }
