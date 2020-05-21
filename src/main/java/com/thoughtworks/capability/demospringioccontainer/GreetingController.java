e945244abc946af27697b3bfc259108b84b92639
Xiaofeng Wang <xfun68@gmail.com>
2020-05-21 11:05:47 +0800

    new instance of GreetingService inside controller

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
@@ -1,6 +1,5 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,9 +8,8 @@ public class GreetingController {
 
     private final GreetingService greetingService;
 
-    @Autowired
-    public GreetingController(GreetingService greetingService) {
-        this.greetingService = greetingService;
+    public GreetingController() {
+        this.greetingService = new GreetingService();
     }
 
     @GetMapping("/greet")
--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
@@ -1,10 +1,5 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
-
-//@Service
-@Component
 public class GreetingService {
 
     String sayHi() {
