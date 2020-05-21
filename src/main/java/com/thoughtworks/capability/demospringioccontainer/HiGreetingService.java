1ebd4db2d855ea3f49b630b3691b69fbd7443518
Xiaofeng Wang <xfun68@gmail.com>
2020-05-21 16:32:26 +0800

    use @Primary

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/HiGreetingService.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/HiGreetingService.java
@@ -1,7 +1,9 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
+@Primary
 @Component
 public class HiGreetingService implements GreetingService {
 
