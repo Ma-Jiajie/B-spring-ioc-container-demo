a2783b1d11034401765afded187f573fa5814991
Xiaofeng Wang <xfun68@gmail.com>
2020-05-21 09:53:08 +0800

    First commit

---
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/DemospringioccontainerApplication.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.capability.demospringioccontainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemospringioccontainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemospringioccontainerApplication.class, args);
+	}
+
+}
